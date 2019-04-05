9e91944bcc3866a73f38224de09943dc5bf005e3
Jakub Dolezal <jakub@dolez.al>
2019-04-05 14:01:11 +0300

    add tests

---
+++ src/test/java/fi/vamk/tka/reactspring/model/GroupTest.java
@@ -0,0 +1,11 @@
+package fi.vamk.tka.reactspring.model;
+
+import org.junit.Test;
+
+public class GroupTest {
+    private GroupRepository repository;
+    @Test
+    public void testing(){
+        Group item = new Group();
+    }
+}
