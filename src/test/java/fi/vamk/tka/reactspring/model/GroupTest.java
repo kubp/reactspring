2a8e096136863a77041fa19e22f28cf13b404df9
Jakub Dolezal <jakub@dolez.al>
2019-04-10 12:53:46 +0300

    another test

--- src/test/java/fi/vamk/tka/reactspring/model/GroupTest.java
+++ src/test/java/fi/vamk/tka/reactspring/model/GroupTest.java
@@ -1,11 +1,43 @@
 package fi.vamk.tka.reactspring.model;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableJpaRepositories(basePackageClasses = GroupRepository.class)
 public class GroupTest {
+    @Autowired
     private GroupRepository repository;
+
     @Test
-    public void testing(){
+    public void addGroupAndVerifyTest() {
         Group item = new Group();
+        item.setName("Vaasa JUG");
+        repository.save(item);
+        Group found = repository.findByName("Vaasa2 JUG");
+        assertNull(found);
+        if (found != null) {
+            assertEquals(found.getName(), item.getName());
+        }
     }
+
+    @Test
+    public void justTesting() {
+        System.out.println("---------test,test,test-------");
+    }
+
+    @Test
+    public void againTesting() {
+        System.out.println("---------Timo, Timo, Timo-------");
+    }
+
 }
